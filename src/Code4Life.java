bf56276f20d999e74efcf5e151ebe26854578060
GamingAuthor <league@wts13.attlocal.net>
2018-04-05 19:59:33 -0700

    Fibonacci, Code 4 Life, Goofy Names, Guessing Pi

--- src/Code4Life.java
+++ src/Code4Life.java
@@ -1,3 +1,4 @@
+
 /*
  *    Copyright (c) The League of Amazing Programmers 2013-2017
  *    Level 0
@@ -7,30 +8,38 @@ import java.io.File;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
-
+import javax.swing.JOptionPane;
 
 public class Code4Life {
-	/*
-     * Ask the user how many hours they spent coding this week.
-     * 
-     * 1. If it's 3 or more, tell them they're a Code Ninja.
-     * 
-     * 2. If it's less than 2, tell them to stop watching YouTube and write code instead.
-     * 
-     * 3. If it's more than 5, play the Batman theme song.
-     */
+	public static void main(String[] args) {
 
+		// Ask the user how many hours they spent coding this week.
+		String hours = JOptionPane.showInputDialog("How many hours did you spend coding this week?");
+		int numH = Integer.parseInt(hours);
+		// 1. If it's 3 or more, tell them they're a Code Ninja.
+		if (numH >= 3) {
+			JOptionPane.showMessageDialog(null, "You're a code Ninja!");
+		}
+		// 2. If it's less than 2, tell them to stop watching Twitch streams and write
+		// code instead.
+		else if (numH <= 2) {
+			JOptionPane.showMessageDialog(null, "Stop watching Twitch streams and write code instead.");
+		}
+		// 3. If it's more than 5, play the Batman theme song.
+		if (numH >= 5) {
+			playBatmanTheme();
+		}
+	}
 
-    private static void playBatmanTheme() {
-    	try {
-   		 Clip clip = AudioSystem.getClip();
-   		 clip.open(AudioSystem.getAudioInputStream(Code4Life.class.getResource("batman.wav")));
-   		 clip.start();
-   		 Thread.sleep(60002);
-   	} catch (Exception ex) {
-     	ex.printStackTrace();
-   	}
-    }
-
+	private static void playBatmanTheme() {
+		try {
+			Clip clip = AudioSystem.getClip();
+			clip.open(AudioSystem.getAudioInputStream(Code4Life.class.getResource("batman.wav")));
+			clip.start();
+			Thread.sleep(60002);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
 
 }
--- src/GuessingPi.java
+++ src/GuessingPi.java
@@ -1,3 +1,4 @@
+
 /*
  *    Copyright (c) The League of Amazing Programmers 2013-2017
  *    Level 0
@@ -7,32 +8,38 @@ import javax.swing.JOptionPane;
 
 public class GuessingPi {
 	// 1. Make a main method and make sure your program can run
+	public static void main(String[] args) {
 
 		// 2. Make a String variable to hold the value of Pi.
-		//    Get the first few digits from http://www.piday.org/million/.
-
+		// Get the first few digits from http://www.piday.org/million/.
+		String pi = "3.14159265358979323846";
 		// 3. Print out the first 3 digits of Pi to the console.
-		//    The first value is "pi.charAt(0)", the second is "pi.charAt(1)", etc.
-		//    Run your program to see how this works.
-
+		// The first value is "pi.charAt(0)", the second is "pi.charAt(1)", etc.
+		// Run your program to see how this works.
+		System.out.println("" + pi.charAt(0) + pi.charAt(1) + pi.charAt(2));
 		// 9. If you want to give the user more than one chance to guess,
-		//    put a for loop around steps 4-8.
+		// put a for loop around steps 4-8.
 
-			// 4. Create a for loop that will step through each digit of pi (steps 5-8)
-			//    NOTE: The number of digits in your string will be
-			//       yourStringVariable.length()
+		// 4. Create a for loop that will step through each digit of pi (steps 5-8)
+		// NOTE: The number of digits in your string will be
+		// yourStringVariable.length()
+		for (int i = 0; i < pi.length(); i++) {
 
-				// 5. Ask the user for the NEXT digit of pi.
-
-				// 6. Compare the user's input to the next digit of your pi variable 
-				//    (look at step 3 for a clue).
-				//    HINT: use charAt(0) to get 1st char of user input String 
-
-					// 7. If they are correct, print out "correct".
-
-					// 8. If they are not, print out "incorrect" and tell them
-					//    to start over. Use 'break;' to break out of the loop.
-
+			// 5. Ask the user for the NEXT digit of pi.
+			String hi = JOptionPane.showInputDialog("What is the next digit of pi?");
+			// 6. Compare the user's input to the next digit of your pi variable
+			// (look at step 3 for a clue).
+			// HINT: use charAt(0) to get 1st char of user input String
+			if (hi.equals(pi.charAt(i) + "")) {
+				// 7. If they are correct, print out "correct".
+				System.out.println("Correct!:D");
+			}
+			// 8. If they are not, print out "incorrect" and tell them
+			// to start over. Use 'break;' to break out of the loop.
+			else {
+				System.out.println("Wrong :O");
+				break;
+			}
+		}
+	}
 }
-
-
