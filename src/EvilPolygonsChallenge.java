146cd849a7d6080ad1156801b8a576384d832642
league <league@wts13.attlocal.net>
2018-04-19 19:57:09 -0700

    Evil Polygons Challenge

--- src/EvilPolygonsChallenge.java
+++ src/EvilPolygonsChallenge.java
@@ -5,23 +5,49 @@ import javax.swing.JOptionPane;
 import org.jointheleague.graphical.robot.Robot;
 
 public class EvilPolygonsChallenge {
-	
+
 	public static void main(String[] args) {
-		//1. Create a robot
+		// 1. Create a robot
+		Robot robot = new Robot();
+		// 2. Set the speed to 100
+		robot.setSpeed(100);
+		int colorChoice = JOptionPane.showOptionDialog(null, "Choose a color.", "Color Chooser",
+				JOptionPane.INFORMATION_MESSAGE, JOptionPane.DEFAULT_OPTION, null,
+				new String[] { "Red", "Green", "Blue" }, 0);
 
-		//2. Set the speed to 100
+		// 3. Set the pen color of the robot according to the value of the colorChoise
+		// variable
+		if (colorChoice == 0) {
+			robot.setPenColor(175, 0, 0);
+		} else if (colorChoice == 1) {
+			robot.setPenColor(0, 175, 0);
+		} else if (colorChoice == 2) {
+			robot.setPenColor(0, 0, 175);
+		}
+		// 4. Ask the use how many polygons they want to be drawn.
+		String poly = JOptionPane.showInputDialog("How many polygons do you want to be drawn?");
+		int polyNum = Integer.parseInt(poly);
+		// 5. Use the robot to draw the number of polygons the user requested.
+		for (int i = 0; i < polyNum; i++) {
+			robot.hide();
+			robot.penDown();
+			drawPoly(robot);
+			robot.penUp();
+			robot.move(55);
+		}
+		// 6. Make it so your shapes do not overlap
 
-		int colorChoice=JOptionPane.showOptionDialog(null, "hello", "title", JOptionPane.INFORMATION_MESSAGE, JOptionPane.DEFAULT_OPTION, null, new String[]{"Color 1","Color 2", "Color 3"}, 0);
-		
-		//3. Set the pen color of the robot according to the value of the colorChoise variable
-		
-		//4. Ask the use how many polygons they want to be drawn.
-		
-		//5. Use the robot to draw the number of polygons the user requested.
-		
-		//6. Make it so your shapes do not overlap
+		// 7. Challenge: add more colors to the Option Dialog.
+	}
 
-		//7. Challenge: add more colors to the Option Dialog.
+	static void drawPoly(Robot robot) {
+		robot.move(50);
+		robot.turn(90);
+		robot.move(50);
+		robot.turn(90);
+		robot.move(50);
+		robot.turn(90);
+		robot.move(50);
+		robot.turn(90);
 	}
 }
-
